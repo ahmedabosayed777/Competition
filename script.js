f5cbee8cae24239bf2b33ccb57bbbad21f5cd78f
ahmedabosayed777 <ahmedabosayed258@gmail.com>
2025-07-19 21:53:21 +0300

    final sections

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// script.js
-// Navbar scroll effect
+// Navbar scroll
 window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 50) {
